3a33b03e5ac0bf958b3d5aa885006b09d9cf70a5
ripter <ripter001@gmail.com>
2014-09-21 11:50:43 -0700

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
 /* global require, _, Phaser */
 
-var gameConfig = require('./game.js');
+var state = {
+  // load game assests.
+  preload: function() {
 
-var game = new Phaser.Game(1136, 640, Phaser.AUTO, 'phaser', gameConfig);
+  }
+
+  // called after all assests have loaded.
+  , create: function() {
+    var text = 'Class Invaders';
+    var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+
+    var t = game.add.text(game.world.centerX-300, 0, text, style);
+  }
+
+  // called every tick
+  , update: function() {
+
+  }
+
+  // render any post sprite effects.
+  , render: function() {
+
+  }
+};
+
+var game = new Phaser.Game(1136, 640, Phaser.AUTO, 'phaser', state);
